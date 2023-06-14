3069d6d4656d921ac61a11c2e8ee18e023ac6699
epinatci <128381420+epinatci@users.noreply.github.com>
2023-06-14 15:22:29 +0300

    tamamlandi

--- src/bilesenler/Gonderiler/Gonderiler.js
+++ src/bilesenler/Gonderiler/Gonderiler.js
@@ -9,7 +9,10 @@ const Gonderiler = (props) => {
   return (
     <div className="posts-container-wrapper">
       {/* gönderiler dizisini işleyip her döngüde bir Gönderi bileşeni çağırın*/}
-      {/* Gönderi'nin çağırılmasında hangi propları kullanmanız gerektiğine dikkat edin! */}
+      {console.log(gonderiler)}
+      {gonderiler.map((item) =>(
+        <Gonderi gonderi= {item} gonderiyiBegen ={gonderiyiBegen}/>
+      ))}
     </div>
   );
 };
